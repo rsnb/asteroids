c79e79c394568706d8d4d40f93b4e2b7665af5d0
Nathaniel Rosenbloom <112800403+nathanielclassiqio@users.noreply.github.com>
2024-11-20 23:00:25 +0200

    Asteroids

--- main.py
+++ main.py
@@ -1,19 +1,45 @@
+from time import time_ns
+
 import pygame
 
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import *
+from player import Player
+
 
 def main():
 	num_pass, num_fail = pygame.init()
 	print(f"Stats: passing {num_pass}, failing {num_fail}")
 
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	clock = pygame.time.Clock()
+	dt = 0
+
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+	asteroids = pygame.sprite.Group()
+
+	Player.containers = (updatable, drawable)
+	Asteroid.containers = (asteroids, updatable, drawable)
+	AsteroidField.containers = (updatable)
+
+	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+	asteroidField = AsteroidField()
+
 
 	while True:
-		screen.fill((0,0,0))
-		pygame.display.flip()
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
+
+		screen.fill((0,0,0))
+		for sprite in updatable:
+			sprite.update(dt)
+		for sprite in drawable:
+			sprite.draw(screen)
+		pygame.display.flip()
+		dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
 	main()
